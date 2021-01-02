c57b83f2216e7652a1d5857728a741e31ecb7d59
RigvedBhavsar <rigvedbhavsar@gmail.com>
2021-01-02 13:43:52 +0530

    Instagram working

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    resetToken:String,
+    expireToken:Date,
+    pic:{
+     type:String,
+     default:"https://res.cloudinary.com/dmynbwuhm/image/upload/v1608975085/NoImage_grklek.jpg"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
+
+mongoose.model("User",userSchema)
